bd7c1f25a2f6d2c9a0363e23e65f3ae978cd1853
Jason Early <theoffice@gruntmonkey.com>
2018-09-05 20:07:26 -0500

    Added section and wave components

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -7,35 +7,35 @@ class Header extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-   hasScrolled: false
- }
-}
-
-componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll)
-}
-handleScroll = (event) => {
-  const scrollTop = window.pageYOffset
-
-  if (scrollTop > 50) {
-    this.setState({ hasScrolled: true })
-  } else {
-    this.setState({ hasScrolled: false })
+      hasScrolled: false
+    }
   }
-}
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
 
   render() {
     return (
       // Move all content between parentheses to here.
       <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
       <div className="HeaderGroup">
-    	<Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-    	<Link to="/courses">Courses</Link>
-    	<Link to="/downloads">Downloads</Link>
-    	<Link to="/workshops">Workshops</Link>
-    	<Link to="/buy"><button>Buy</button></Link>
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
       </div>
-    </div>
+      </div>
     )
   }
 }
