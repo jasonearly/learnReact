157b198d7ea455fff082bd6fb1edc88f85619c9c
Jason Early <theoffice@gruntmonkey.com>
2018-09-16 08:30:30 -0500

    trying gh-pages deploy with gatsby

--- my-app/gatsby-config.js
+++ my-app/gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
 	title: 'Design+Code 3',
 	description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
-	keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
+	keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin',
+  pathPrefix: "/learnReact"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
