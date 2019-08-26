96cc84b007fc9f7847a903509e4a8f47ed8060f7
Jason Early <theoffice@gruntmonkey.com>
2019-08-26 16:30:07 -0500

    make components and layout

---
+++ reactUXCourse/src/components/card/card.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './card.scss'
+
+const Card = props => (
+    <div className="Card">
+      <img src={props.image} alt={props.alt}/>
+        <h3>{props.title}</h3>
+        <p>{props.text}</p>
+
+    </div>
+)
+
+export default Card
--- reactUXCourse/src/pages/index.js
+++ reactUXCourse/src/pages/index.js
@@ -1,20 +1,61 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/layouts/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
+import Hero from '../components/hero/hero'
+import Card from '../components/card/card'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="Hero">
-      <div className="HeroGroup">
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-          <Link to="/page-2/">Go to page 2</Link>
-      </div>
+    <Hero />
+
+    <div className="Cards">
+      <h2>Test Heading</h2>
+
+<div className="CardGroup">
+      <Card
+        title="Title"
+        text="paragraph copy"
+        image={require('../images/background.png') }
+        alt="alt copy"
+        />
+
+      <Card
+        title="Title"
+        text="paragraph copy"
+        image={require('../images/background.png') }
+        alt="alt copy"
+        />
+
+      <Card
+        title="Title"
+        text="paragraph copy"
+        image={require('../images/background.png') }
+        alt="alt copy"
+        />
+
+
+      <Card
+        title="Title"
+        text="paragraph copy"
+        image={require('../images/background.png') }
+        alt="alt copy"
+        />
+
+
+      <Card
+        title="Title"
+        text="paragraph copy"
+        image={require('../images/background.png') }
+        alt="alt copy"
+        />
+
     </div>
+</div>
+
 
 
   </Layout>
